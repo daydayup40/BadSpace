02e8833ce326cbfa102ea300290d4a4e6702d158
keshuichonglx <lixiang890217@163.com>
2017-01-10 22:37:00 +0800

    Update setup_JDK .sh

--- Install_JDK_Ubuntu/setup_JDK .sh
+++ Install_JDK_Ubuntu/setup_JDK .sh
@@ -9,9 +9,9 @@ JAVA_VERSIONNAME=jdk1.8.0_65
 JAVA_ZIPNAME=jdk-8u65-linux-x64.tar.gz
 JAVA_SETUP_LINK_PATH=$JAVA_JDK_PATH/java
 
-echo "===============  setup jdk  ==============="
 sudo mkdir $JAVA_JDK_PATH
-sudo tar zxvf $CURDIR/$JAVA_ZIPNAME -C $JAVA_JDK_PATH
+echo "tar....."
+sudo tar zxvf $CURDIR/$JAVA_ZIPNAME -C $JAVA_JDK_PATH > null
 sudo ln -s $JAVA_JDK_PATH/$JAVA_VERSIONNAME $JAVA_SETUP_LINK_PATH
 
 sudo echo -e "" >> /etc/profile
@@ -22,12 +22,12 @@ sudo echo -e "export PATH=\${JAVA_HOME}/bin:\$PATH" >> /etc/profile
 sudo source /etc/profile
 sudo . /etc/profile
 
-sudo echo -e "" >> ~/.bashrc
-sudo echo -e "export JAVA_HOME=$JAVA_SETUP_LINK_PATH" >> ~/.bashrc
-sudo echo -e "export JRE_HOME=\${JAVA_HOME}/jre" >> ~/.bashrc
-sudo echo -e "export CLASSPATH=.:\${JAVA_HOME}/lib:\${JRE_HOME}/lib" >> ~/.bashrc
-sudo echo -e "export PATH=\${JAVA_HOME}/bin:\$PATH" >> ~/.bashrc
-sudo source ~/.bashrc
+sudo echo -e "" >> /root/.bashrc
+sudo echo -e "export JAVA_HOME=$JAVA_SETUP_LINK_PATH" >> /root/.bashrc
+sudo echo -e "export JRE_HOME=\${JAVA_HOME}/jre" >> /root/.bashrc
+sudo echo -e "export CLASSPATH=.:\${JAVA_HOME}/lib:\${JRE_HOME}/lib" >> /root/.bashrc
+sudo echo -e "export PATH=\${JAVA_HOME}/bin:\$PATH" >> /root/.bashrc
+sudo source /root/.bashrc
 
 echo -e "" >> ~/.bashrc
 echo -e "export JAVA_HOME=$JAVA_SETUP_LINK_PATH" >> ~/.bashrc
